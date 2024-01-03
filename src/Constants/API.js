a188735c0edcdda221bf2aeb9fe7548f9794670a
safhadk <safadkothikkal0880@gmail.com>
2024-01-03 22:54:47 +0530

    domain changed

--- src/Constants/API.js
+++ src/Constants/API.js
@@ -1,8 +1,8 @@
-// export const userAPI = "https://royalcars.onrender.com/";
+export const userAPI = "royalcars-server.vercel.app/";
 // export const adminAPI = "https://royalcars.onrender.com/admin/";
 // export const ownerAPI = "https://royalcars.onrender.com/owner/";
 
-export const userAPI = "http://localhost:5000/";
+// export const userAPI = "http://localhost:5000/";
 export const adminAPI = "http://localhost:5000/admin/";
 export const ownerAPI = "http://localhost:5000/owner/";
 
