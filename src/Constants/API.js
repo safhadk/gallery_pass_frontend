f8c7fca71122d2de463b5d298c17ab5b4351d9a1
safhadk <safadkothikkal0880@gmail.com>
2024-01-04 13:41:29 +0530

    updates

--- src/Constants/API.js
+++ src/Constants/API.js
@@ -1,8 +1,8 @@
 export const userAPI = "royalcars-server.vercel.app/";
-// export const adminAPI = "https://royalcars.onrender.com/admin/";
-// export const ownerAPI = "https://royalcars.onrender.com/owner/";
+export const adminAPI = "https://royalcars.onrender.com/admin/";
+export const ownerAPI = "https://royalcars.onrender.com/owner/";
 
 // export const userAPI = "http://localhost:5000/";
-export const adminAPI = "http://localhost:5000/admin/";
-export const ownerAPI = "http://localhost:5000/owner/";
+// export const adminAPI = "http://localhost:5000/admin/";
+// export const ownerAPI = "http://localhost:5000/owner/";
 
